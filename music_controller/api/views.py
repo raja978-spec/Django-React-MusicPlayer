ffc7010e056e7a93ed1fd22ed18ef35b19659c97
raja-spec <rraja48569@gmail.com>
2023-07-27 19:53:52 +0530

    Adding ViewPoint

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 # Create your views here. 
-def main(request):
-    return HttpResponse("<h1> Hello world </h1>")
+
+# This view gives a view to create object to the
+
+class RoomView(generics.CreateAPIView):
+    queryset=Room.objects.all()
+    serializer_class=RoomSerializer
+
+# This gives list of created object
+'''
+class RoomView(generics.ListAPIView):
+    queryset=Room.objects.all()
+    serializer_class=RoomSerializer
+'''
